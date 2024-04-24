cdf9bd647b9a103b30c5f4cf7d86c45d6c6db7c5
“Fay <faylovepink@gmail.com>
2024-04-24 00:18:58 -0400

    fixed project page loading issue, working on card height and layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import Fade from 'react-reveal/Fade';
+import Fade from 'react-reveal';
 import Header from './Header';
 import endpoints from '../constants/endpoints';
 import ProjectCard from './projects/ProjectCard';
 import FallbackSpinner from './FallbackSpinner';
-import '../css/projects.css'; // Import CSS file
+import '../css/projects.css';
 
-// Define styles object
 const styles = {
   containerStyle: {
     marginBottom: 25,
   },
 };
 
-const Projects = ({ header }) => {
+const Projects = (props) => {
+  const { header } = props;
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -24,24 +24,28 @@ const Projects = ({ header }) => {
     })
       .then((res) => res.json())
       .then((res) => setData(res))
-      .catch((err) => console.error(err));
+      .catch((err) => err);
   }, []);
+
+  const numberOfItems = data ? data.length : 6;
 
   return (
     <>
       <Header title={header} />
-      <div className="section-content-container">
-        <Container style={styles.containerStyle}>
-          <Row xs={1} sm={1} md={2} lg={3} className="g-4">
-            {data?.projects?.map((project) => (
-              <Fade key={project.title}>
-                <ProjectCard project={project} className="project-card" />
-              </Fade>
-            ))}
-          </Row>
-        </Container>
-      </div>
-      <FallbackSpinner />
+      {data
+        ? (
+          <div className="section-content-container">
+            <Container style={styles.containerStyle}>
+              <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+                {data.projects?.slice(0, numberOfItems).map((project) => (
+                  <Fade key={project.title}>
+                    <ProjectCard project={project} />
+                  </Fade>
+                ))}
+              </Row>
+            </Container>
+          </div>
+        ) : <FallbackSpinner /> }
     </>
   );
 };
